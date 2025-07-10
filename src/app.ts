43efe97125ccccab53f76f147839e382f5e98e0d
Md. Habibullah <mdhabibullah.work@gmail.com>
2025-07-10 14:34:20 +0600

    setup completed

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'PH tour management server is running'
+    })
+})
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-console */
+import { Server } from 'http';
+import mongoose from 'mongoose'
+import app from './app';
+import { envVars } from './app/config/env';
+
+let server: Server;
+
+const startServer = async () => {
+    try {
+        await mongoose.connect(envVars.DB_URL)
+
+        console.log("connected to mongodb")
+
+        server = app.listen(5000, () => {
+            console.log('server is listening to port 5000')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
+
+// Unhandled rejection error handle
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled rejection detected..server is shutting down..", err)
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
+
+// uncaught exception error handle
+process.on("uncaughtException", (err) => {
+    console.log("uncaught Exception detected..server is shutting down..", err)
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received..server is shutting down..")
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+})
+
+// Promise.reject(new Error("i forgot to catch this error"))
+/**
+* Unhandled rejection error
+* uncaught exception error
+* signal termination error - sigturm
+*/
